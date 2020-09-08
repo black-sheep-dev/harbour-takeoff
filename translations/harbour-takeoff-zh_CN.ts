130b15bbe538ff1e8797d13cfe9dcf611c936b3e
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-09-08 15:16:09 +0800

    Create harbour-takeoff-zh_CN.ts

---
+++ translations/harbour-takeoff-zh_CN.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Sources</source>
+        <translation>源</translation>
+    </message>
+    <message>
+        <source>Donations</source>
+        <translation>捐赠</translation>
+    </message>
+    <message>
+        <source>Buy me a beer</source>
+        <translation>给我买瓶啤酒</translation>
+    </message>
+    <message>
+        <source>Takeoff is an autostart manager for launching apps after device boot.</source>
+        <translation>Takeoff 是用于控制设备启动后的软件自启情况管理器。</translation>
+    </message>
+</context>
+<context>
+    <name>ActiveAppsPage</name>
+    <message>
+        <source>Manage</source>
+        <translation>管理</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Active Apps</source>
+        <translation>允许自启的软件</translation>
+    </message>
+</context>
+<context>
+    <name>ManagerPage</name>
+    <message>
+        <source>Manage Apps</source>
+        <translation>管理软件</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>刷新</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>重置</translation>
+    </message>
+</context>
+</TS>
