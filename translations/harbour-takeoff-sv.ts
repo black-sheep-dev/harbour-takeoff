f1908de4bde1ebe9330705b1968e7c21862890f5
Åke Engelbrektson <eson57@users.noreply.github.com>
2020-09-11 08:02:05 +0200

    Create harbour-takeoff-sv.ts
    Add Swedish translation

---
+++ translations/harbour-takeoff-sv.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Sources</source>
+        <translation>Källor</translation>
+    </message>
+    <message>
+        <source>Donations</source>
+        <translation>Donationer</translation>
+    </message>
+    <message>
+        <source>Buy me a beer</source>
+        <translation>Bjud mig på en öl</translation>
+    </message>
+    <message>
+        <source>Takeoff is an autostart manager for launching apps after device boot.</source>
+        <translation>Takeoff är en autostarthanterare för att starta appar efter enhetsstart.</translation>
+    </message>
+</context>
+<context>
+    <name>ActiveAppsPage</name>
+    <message>
+        <source>Manage</source>
+        <translation>Hantera</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Active Apps</source>
+        <translation>Aktiva appar</translation>
+    </message>
+    <message>
+        <source>Move up</source>
+        <translation>Flytta upp</translation>
+    </message>
+    <message>
+        <source>Move down</source>
+        <translation>Flytta ner</translation>
+    </message>
+</context>
+<context>
+    <name>ManagerPage</name>
+    <message>
+        <source>Manage Apps</source>
+        <translation>Hantera appar</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Återställ</translation>
+    </message>
+</context>
+</TS>
